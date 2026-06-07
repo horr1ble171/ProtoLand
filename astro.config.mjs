2be04c55e1535be5ef15bf2a53d60e6b23361509
horr1ble171 <evangulovnikita@gmail.com>
2026-06-07 20:19:22 +0200

    Fix OG image URL with correct site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 
 export default defineConfig({
+  site: 'https://proto-land-two.vercel.app',
   integrations: [tailwind()],
   output: 'static',
   adapter: vercel(),
